e501ce92f889638a3ee5b01ff0b1085d3b7b6390
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-12 20:02:44 -0300

    FIX: rewrite operator handler to handle negative values

--- src/App.js
+++ src/App.js
@@ -8,16 +8,36 @@ class App extends Component {
     this.state = {
       output: '0',
       input: '',
-      isSym: false
+      isSym: true
     }
   }
-  reset = () => this.setState({ output:'0', input:'', isSym: false });
+  reset = () => this.setState({ output:'0', input:'', isSym: true });
 
   numbers = e => {
     let evt = e.target.value;
-    this.setState(prevState => ({ input: prevState.input === '0' ? evt : prevState.input + evt }));
+    this.setState(prevState => ({ input: prevState.input === '0' ? evt : prevState.input + evt, isSym: false }));
   };
 
+  operators = e => {
+    let evt = e.target.value;
+    this.setState(prevState => {
+      if (evt === '-'){
+        if (prevState.input === '' || /\d-?$/.test(prevState.input)){
+          return ({ input: prevState.input + evt })
+        }
+      } else {
+        if (/\d$/.test(prevState.input)){
+          return ({ input: prevState.input + evt })
+        } else if (/\d[-+*/]$/.test(prevState.input)){
+          return ({ input: prevState.input.slice(0,-1) + evt })
+        }
+      }
+    })
+  };
+
+  // const decimal = e => {
+  //
+  // }
 
 
 
